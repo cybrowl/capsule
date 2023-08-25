fd7a2a98873b9bf9b5c6e855ae21e37362e01fe9
cyberowl <cybrowl@protonmail.com>
2023-08-24 23:52:50 -0700

    add: grid layout and styling to the page
    - Add CSS classes for grid layout and positioning
    - Set up a 12-column grid with responsive behavior
    - Add background colors for different sections of the page
    - Use object-fit: cover for the image in the left column

--- src/ui/routes/+page.svelte
+++ src/ui/routes/+page.svelte
@@ -6,9 +6,15 @@
 	<title>Capsule</title>
 </svelte:head>
 
-<main>
-	<div>
-		<h1 class="text-red-500">hello</h1>
+<main class="grid grid-cols-12 h-screen">
+	<!-- Left column with the image -->
+	<div class="col-span-6 relative">
+		<img src="fish_bg.jpeg" alt="Description" class="absolute inset-0 w-full h-full object-cover" />
+	</div>
+	<div class="col-span-6 bg-red-500 grid grid-rows-2">
+		<!-- 6 columns content for the right side -->
+		<div class="row-span-1 bg-green-500">Top content here.</div>
+		<div class="row-span-1 bg-yellow-500">Bottom content here.</div>
 	</div>
 </main>
 
