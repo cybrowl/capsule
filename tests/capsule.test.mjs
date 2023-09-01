82af8186cc37bb3c2b8744a4106ecfb8640222ea
cyberowl <cybrowl@protonmail.com>
2023-08-31 23:47:51 -0700

    refactor: tests for Capsule and FileStorage actors
    - Refactored the test file `capsule.test.mjs` to improve readability and maintainability.
    - Added a new import statement for the `fs` module from Node.js.
    - Updated comments to clarify that encryption testing is done in the browser.
    - Added two new test cases:
      - `FileStorage[random].create_chunk(): with 6MB IMG file #ok -> chunk_ids`: Tests the creation of chunks for a 6MB image file and verifies that chunk IDs are generated successfully.
      - `FileStorage[random].commit_batch(): with 6MB IMG file #ok -> asset_id`: Tests the commit of chunks as an asset and verifies that an asset ID is returned.
    
    The changes aim to enhance the existing tests for Capsule and FileStorage actors by adding more comprehensive coverage.

--- tests/capsule.test.mjs
+++ tests/capsule.test.mjs
@@ -5,11 +5,12 @@ import { capsule_canister_id, file_storage_canister_id } from './actor_canister_
 import { capsule_interface, file_storage_interface } from './actor_interface.mjs';
 import { getActor } from './actor.mjs';
 import { v4 as uuidv4 } from 'uuid';
+import fs from 'node:fs';
 
 config();
 
 // NOTE: encryption is not tested because too many dep on browser
-// Testing is done in browser
+// encryption testing is done in browser
 
 const parseIdentity = (privateKeyHex) => {
 	const privateKey = Uint8Array.from(Buffer.from(privateKeyHex, 'hex'));
@@ -27,6 +28,8 @@ let capsule_actor = {};
 let file_storage_actor = {};
 
 let capsule_x = '';
+let asset_id_x = '';
+let chunk_ids = [];
 
 test('Setup Actors', async function () {
 	console.log('=========== Capsule ===========');
@@ -58,6 +61,63 @@ test('Setup Actors', async function () {
 		const version_r = await file_storage_actor.random.version();
 
 		t.equal(version_r, 4n);
+	});
+
+	test('FileStorage[random].create_chunk(): with 6MB IMG file #ok -> chunk_ids', async function (t) {
+		const uploadChunk = async ({ chunk, order }) => {
+			return file_storage_actor.random.create_chunk(chunk, order);
+		};
+
+		const file_path = 'tests/data/poked_2.jpeg';
+
+		const asset_buffer = fs.readFileSync(file_path);
+
+		const asset_unit8Array = new Uint8Array(asset_buffer);
+
+		const promises = [];
+		const chunkSize = 2000000;
+
+		for (let start = 0, index = 0; start < asset_unit8Array.length; start += chunkSize, index++) {
+			const chunk = asset_unit8Array.slice(start, start + chunkSize);
+
+			promises.push(
+				uploadChunk({
+					chunk,
+					order: index
+				})
+			);
+		}
+
+		chunk_ids = await Promise.all(promises);
+
+		const hasChunkIds = chunk_ids.length > 2;
+
+		t.equal(hasChunkIds, true);
+	});
+
+	test('FileStorage[random].commit_batch(): with 6MB IMG file #ok -> asset_id', async function (t) {
+		const asset_filename = 'poked_2.jpeg';
+		const asset_content_type = 'image/jpeg';
+		const options = {
+			filename: asset_filename,
+			content_encoding: { Identity: null },
+			content_type: asset_content_type
+		};
+
+		const ids_sorted = chunk_ids.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+
+		const { ok: asset_id, err: error } = await file_storage_actor.random.commit_batch(
+			ids_sorted,
+			options
+		);
+
+		asset_id_x = asset_id;
+
+		const { ok: asset } = await file_storage_actor.random.get(asset_id);
+
+		t.equal(error, undefined);
+		t.equal(asset.filename, 'poked_2.jpeg');
+		t.equal(asset.content_type, 'image/jpeg');
 	});
 
 	test('Capsule[random].check_capsule_exists(): with invalid id => #err - false', async function (t) {
@@ -95,6 +155,43 @@ test('Setup Actors', async function () {
 	test('Capsule[random].get_capsule(): with valid id => #ok - capsule', async function (t) {
 		const { ok: capsule, err: err_creating } = await capsule_actor.random.get_capsule(capsule_x);
 
-		console.log('capsule: ', capsule);
+		t.ok(capsule.id, 'Capsule should have an ID');
+
+		t.deepEqual(capsule.files, [], 'Capsule files should be an empty array');
+
+		t.deepEqual(capsule.authorized, [], 'Capsule authorized should be an empty array');
+
+		t.equal(capsule.locked_start, 0n, 'Capsule locked_start should be 0n');
+		t.equal(capsule.locked_minutes, 0n, 'Capsule locked_minutes should be 0n');
+
+		t.equal(err_creating, undefined, 'There should be no error while fetching the capsule');
+	});
+
+	test('Capsule[random].add_file(): with valid asset and capsule => #ok - AddedFile', async function (t) {
+		const { ok: added_file, err: err_adding } = await capsule_actor.random.add_file(
+			capsule_x,
+			asset_id_x
+		);
+
+		t.deepEqual(added_file, { AddedFile: true }, 'The file should be successfully added');
+
+		t.equal(err_adding, undefined, 'There should be no error while adding the file');
+	});
+
+	test('Capsule[random].get_capsule(): with valid id => #ok - capsule', async function (t) {
+		const { ok: capsule, err: err_creating } = await capsule_actor.random.get_capsule(capsule_x);
+
+		t.ok(capsule.id, 'Capsule should have an ID');
+
+		t.deepEqual(capsule.files.length, 1, 'Capsule should contain exactly one file');
+		t.equal(
+			capsule.files[0].content_type,
+			'image/jpeg',
+			"File content type should be 'image/jpeg'"
+		);
+		t.equal(capsule.files[0].filename, 'poked_2.jpeg', "File filename should be 'poked_2.jpeg'");
+		t.deepEqual(capsule.authorized, [], 'Capsule authorized should be an empty array');
+		t.equal(capsule.locked_start, 0n, 'Capsule locked_start should be 0n');
+		t.equal(capsule.locked_minutes, 0n, 'Capsule locked_minutes should be 0n');
 	});
 });
