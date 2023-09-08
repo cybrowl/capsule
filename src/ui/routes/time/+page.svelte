9e04e2c0812676569c35a37724e44e1ee14dbcf9
cyberowl <cybrowl@protonmail.com>
2023-09-08 04:54:13 -0700

    add: new time capsule page with dynamic routing and UUID generation
    This commit adds a new Svelte component for the time capsule page. The component includes a script that imports the `onMount` function from the 'svelte' library, as well as other necessary dependencies. On mount, the script checks if a `capsule_id` parameter is present in the URL. If not, it generates a UUID using the `uuidv4` function and redirects to a new route with the generated ID. The component also sets the title of the page to "Capsule" using `<svelte:head>`.

---
+++ src/ui/routes/time/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { v4 as uuidv4 } from 'uuid';
+	import { page } from '$app/stores';
+
+	onMount(() => {
+		let { capsule_id } = $page.params;
+
+		if (!capsule_id) {
+			capsule_id = uuidv4();
+
+			const capsule_route = '/time/' + capsule_id;
+			goto(capsule_route);
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>Capsule</title>
+</svelte:head>
